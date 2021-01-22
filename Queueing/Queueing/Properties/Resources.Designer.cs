851a4054013fada191d13f7fb8d15f3cf9b1ec6d
WoodsPentagon <50472091+WoodsPentagon@users.noreply.github.com>
2021-01-22 21:43:14 +0800

    renamed receipt project

--- Queueing/Queueing/Properties/Resources.Designer.cs
+++ Queueing/Queueing/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Queueing.Properties {
+namespace QueReceipt.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Queueing.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Queueing.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("QueReceipt.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
